978aa8f5bd816c08f2b02c5241129c2cb2c7dabd
renegade27 <foemaster@live.com>
2019-06-24 15:52:46 -0500

    POM.xml and Application properties setup. Postgres DB setup, and config setup to work with that db.

---
+++ empconvo/src/main/java/naught/local/empconvo/EmpconvoApplication.java
@@ -0,0 +1,13 @@
+package naught.local.empconvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpconvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpconvoApplication.class, args);
+    }
+
+}
