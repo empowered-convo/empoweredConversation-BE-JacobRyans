8d210046a043abb6060677086432521c101e3d87
renegade27 <foemaster@live.com>
2019-06-24 18:51:04 -0500

    Removed authentication configs completely. Setup exception handling without authentication. Created endpoint for getting resources by category id. Created function for interacting with the resource repo to fetch all resources by category id from postgres.

--- empconvo/src/main/java/naught/local/empconvo/EmpconvoApplication.java
+++ empconvo/src/main/java/naught/local/empconvo/EmpconvoApplication.java
@@ -2,8 +2,6 @@ package naught.local.empconvo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
