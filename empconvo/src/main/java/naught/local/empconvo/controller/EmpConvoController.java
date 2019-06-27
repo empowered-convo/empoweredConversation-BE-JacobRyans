402ae64c0be28fb353521e22d7c1ddfec4cb6ef5
renegade27 <foemaster@live.com>
2019-06-27 15:51:38 -0500

    Added new Twilio number, and now using env variables with Heroku.

--- empconvo/src/main/java/naught/local/empconvo/controller/EmpConvoController.java
+++ empconvo/src/main/java/naught/local/empconvo/controller/EmpConvoController.java
@@ -83,12 +83,12 @@ public class EmpConvoController {
     @PostMapping(value="/conversations")
     public ResponseEntity<?> createNewConversation(@Valid @RequestBody Conversation newConvo) {
         Conversation createdConvo = convoService.save(newConvo);
-        String ACCOUNT_SID = "AC3004f5000c8be4bc0f62a04d63d5b6d0";
-        String AUTH_TOKEN = "763bfc43915422c9baf19f2e36e2fd9f";
+        String ACCOUNT_SID = System.getenv("TWILIO_SID");
+        String AUTH_TOKEN = System.getenv("TWILIO_TOKEN");
         TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("To", newConvo.getFfnumber()));
-        params.add(new BasicNameValuePair("From", "+19179206969"));
+        params.add(new BasicNameValuePair("From", "+19292393020"));
         params.add(new BasicNameValuePair("Body", createdConvo.getSurvivorname() + " would like to speak with you about a sensitive matter. " + "https://empowered-conversation.netlify.com/conversation/resources/" + "?cid=" + createdConvo.getConversationid() + "&caid=" + createdConvo.getCategory().getCategoryid() ));
         MessageFactory messageFactory = client.getAccount().getMessageFactory();
         try { messageFactory.create(params); } catch(Exception exc) { System.out.println(exc); };
@@ -114,13 +114,13 @@ public class EmpConvoController {
             }
         }
         Conversation createdConvo = convoService.findById(conversationid);
-        String ACCOUNT_SID = "AC3004f5000c8be4bc0f62a04d63d5b6d0";
-        String AUTH_TOKEN = "763bfc43915422c9baf19f2e36e2fd9f";
+        String ACCOUNT_SID = System.getenv("TWILIO_SID");
+        String AUTH_TOKEN = System.getenv("TWILIO_TOKEN");
         TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("To", createdConvo.getSurvivornumber()));
-        params.add(new BasicNameValuePair("From", "+19179206969"));
-        params.add(new BasicNameValuePair("Body", createdConvo.getFfname() + " is ready to speak with you and has read resources to prepare themselves, thank you for trusting in us."));
+        params.add(new BasicNameValuePair("From", "+19292393020"));
+        params.add(new BasicNameValuePair("Body", createdConvo.getFfname() + " is ready to speak with you and has read resources to prepare themselves. Thank you for trusting in the Empowered Conversations service."));
         MessageFactory messageFactory = client.getAccount().getMessageFactory();
         try { messageFactory.create(params); convoService.delete(conversationid); } catch(Exception exc) { throw new EntityNotFoundException(exc.toString()); };
         return new ResponseEntity<>("Conversation " + conversationid + " has been closed.", HttpStatus.OK);
